a2b6f2113c572e5079330e700c769d023d15b37c
Shivam Kapoor <4599890+iamsmkr@users.noreply.github.com>
2024-03-13 05:08:02 +0000

    log activity

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var pause = document.getElementById("pause");
 var finish = document.getElementById("finish");
 var timer = document.getElementById("timer");
 var counter = document.getElementById("counter");
+
 var t;
 var isRunning = false;
 var startTimestamps = [];
@@ -11,6 +12,36 @@ var pauseTimestamps = [];
 var seconds = 0;
 var minutes = 0;
 var hours = 0;
+
+function logActivity() {
+    var weekdays = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+    var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"];
+
+    var activity = "Study";
+    var inactivity = "Break";
+
+    function formatDate(date) {
+        var formattedDate = weekdays[date.getDay()] + ', ' + date.getDate() + ' ' + months[date.getMonth()] + ' ' + date.getFullYear() + ' ' + date.getHours().toString().padStart(2, '0') + ':' + date.getMinutes().toString().padStart(2, '0') + ':' + date.getSeconds().toString().padStart(2, '0');
+        return formattedDate;
+    }
+
+    var start = startTimestamps[startTimestamps.length - 1] || 0;
+    var pause = pauseTimestamps[pauseTimestamps.length - 1] || 0;
+    if (startTimestamps.length !== 0 && pauseTimestamps.length !== 0) {
+        var duration = Math.abs(pause - start);
+        var hours = Math.floor(duration / (1000 * 60 * 60));
+        var minutes = Math.floor((duration % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((duration % (1000 * 60)) / 1000);
+
+        var durationStr =
+            (hours < 10 ? "0" : "") + hours + ":" +
+            (minutes < 10 ? "0" : "") + minutes + ":" +
+            (seconds < 10 ? "0" : "") + seconds;
+
+        // console.log('start', start, 'pause', pause, 'duration', duration);
+        console.log(formatDate(new Date(start)), '|', formatDate(new Date(pause)), '|', durationStr, '|', !isRunning ? activity : inactivity);
+    }
+}
 
 function updateTimer() {
     seconds++;
@@ -49,7 +80,7 @@ function updateCounter() {
 
     for (var i = 1; i < startTimestamps.length; i++) {
         var start = startTimestamps[i];
-        var pause = pauseTimestamps[i - 1];
+        var pause = pauseTimestamps[i - 1] || 0;
         var duration = Math.floor((start - pause) / 1000);
 
         totalBreak += duration;
@@ -69,13 +100,19 @@ function startTimer() {
         startTimestamps.push(new Date().getTime());
         t = setInterval(updateTimer, 1000);
         updateCounter();
+        // console.log('start: ', 'startTimestamps', startTimestamps, 'pauseTimestamps', pauseTimestamps);
+        logActivity();
     }
 }
 
 function pauseTimer() {
-    isRunning = false;
-    clearInterval(t);
-    pauseTimestamps.push(new Date().getTime());
+    if (isRunning) {
+        isRunning = false;
+        clearInterval(t);
+        pauseTimestamps.push(new Date().getTime());
+        // console.log('pause: ', 'startTimestamps', startTimestamps, 'pauseTimestamps', pauseTimestamps);
+        logActivity();
+    }
 }
 
 function finishTimer() {
@@ -87,8 +124,12 @@ function finishTimer() {
     } else if (startTimestamps.length < pauseTimestamps.length) {
         startTimestamps.push(currTime);
         pauseTimestamps.push(currTime);
+    } else {
+        pauseTimestamps.push(currTime);
     }
     updateCounter();
+    // console.log('finish: ', 'startTimestamps', startTimestamps, 'pauseTimestamps', pauseTimestamps);
+    logActivity();
     seconds = 0;
     minutes = 0;
     hours = 0;
